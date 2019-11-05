4ae462268b21f148db0dcdd581289a090a7ca383
Tiberiu G <tiberiu.gaspar98@gmail.com>
2019-11-05 21:29:16 +0200

    android fundamentals week 2

--- app/src/main/java/com/tiber/helloworldatelier/MainActivity.java
+++ app/src/main/java/com/tiber/helloworldatelier/MainActivity.java
@@ -2,12 +2,13 @@ package com.tiber.helloworldatelier;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_week2);
     }
 }
